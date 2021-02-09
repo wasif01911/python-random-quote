25328b15e469b731e304baa3d609f558fc127adf
Wasif <wasif.school@gmail.com>
2021-02-09 12:56:33 +0600

    User input quotes: Project Updated

--- get-quote.py
+++ get-quote.py
@@ -2,13 +2,20 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
+  user_input = input('Add new quotes: ')
+  with open('quotes.txt', 'a') as f:
+    f.write("\n" + user_input)
+
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rndone = random.randint(0, last)
+  rndtwo = random.randint(0, last)
+  print(quotes[rndone], end='')
+  print(quotes[rndtwo], end='')
 
 if __name__== "__main__":
   primary()
